c9abb0c8f5a31165bd21ff5b064f65e4557c35b3
Roman Romanov <rromanov.vk@gmail.com>
2026-03-03 10:08:58 +0500

    init

---
+++ YandexMapKitSwiftUI/YandexMapKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexMapKitSwiftUIApp.swift
+//  YandexMapKitSwiftUI
+//
+//  Created by Roman Romanov on 03.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct YandexMapKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
