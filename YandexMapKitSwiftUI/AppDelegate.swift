8ec3695433701d51dedda44e5186f04e89731aea
Roman Romanov <rromanov.vk@gmail.com>
2026-03-07 23:19:49 +0500

    add local const MAPKIT_API_KEY

--- YandexMapKitSwiftUI/AppDelegate.swift
+++ YandexMapKitSwiftUI/AppDelegate.swift
@@ -10,12 +10,13 @@ import YandexMapsMobile
 
 class AppDelegate: NSObject, UIApplicationDelegate {
 
+	let MAPKIT_API_KEY = "YOUR_API_KEY"
+
 	func application(
 		_ application: UIApplication,
 		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
 	) -> Bool {
-		print("App Did Launch!")
-		YMKMapKit.setApiKey("Ваш API-ключ")
+		YMKMapKit.setApiKey(MAPKIT_API_KEY)
 		// YMKMapKit.setLocale("ru_RU") // если хотим установить конкретную локаль
 		YMKMapKit.sharedInstance()
 		return true
